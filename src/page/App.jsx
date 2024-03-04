36366461a379dd7ac8abae61a319044ba8a415ce
Lucas <lucasbombaron@gmail.com>
2024-03-04 15:28:36 +0100

    page

---
+++ src/page/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//components
+import NavBar from "../components/navBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
