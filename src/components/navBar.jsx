b162909fb1afe54915dace6e60c0dbd4d8e2d750
Lucas <lucasbombaron@gmail.com>
2024-03-04 16:16:28 +0100

    menu burger

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 //assets
 import BurgerIcon from '../assets/icon-menu.svg';
 import LogoImg from '../assets/logo.svg';
 import CartIcon from '../assets/icon-cart.svg';
 import AvatarImg from '../assets/image-avatar.png';
+import CloseIcon from '../assets/icon-close.svg';
 
 function NavBar() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    function openMenu() {
+        setShowMenu(!showMenu);
+        console.log(showMenu);
+    }
+
     return(
-        <div className="w-screen py-2 flex items-center justify-between">
+        <div className="w-screen py-2 flex items-center justify-between bg-white">
             <div className="flex items-center">
-                <button className="h-11 w-11 flex justify-center items-center">
+                <button onClick={() => openMenu()} className="h-11 w-11 flex justify-center items-center">
                     <img src={BurgerIcon} alt="menu" className="h-4"/>
                 </button>
                 <a href="/">
@@ -18,13 +26,32 @@ function NavBar() {
                 </a>
             </div>
             <div className="flex items-center mr-2">
-                <a href="" className="h-11 w-11 flex justify-center items-center">
+                <a href="#" className="h-11 w-11 flex justify-center items-center">
                     <img src={CartIcon} alt="cart" className="h-5" />
                 </a>
-                <a href="" className="h-11 w-11 flex justify-center items-center">
+                <a href="#" className="h-11 w-11 flex justify-center items-center">
                     <img src={AvatarImg} alt="profile" className="h-8" />
                 </a>
             </div>
+            {showMenu && (
+                <div className="absolute top-0 h-screen w-screen">
+                    <div className="absolute top-0 h-screen w-screen bg-black opacity-75"></div>
+                    <div className="absolute top-0 bottom-0 w-2/3 bg-white p-8 text flex flex-col items-start">
+                        <button onClick={() => openMenu()} className="h-11 w-11 flex justify-start items-center">
+                            <img src={CloseIcon} alt="close" />
+                        </button>
+                        <nav>
+                            <ul className="mt-4">
+                                <li className="my-4"><a href="#"><h3 className="font-semibold">Collections</h3></a></li>
+                                <li className="my-4"><a href="#"><h3 className="font-semibold">Men</h3></a></li>
+                                <li className="my-4"><a href="#"><h3 className="font-semibold">Women</h3></a></li>
+                                <li className="my-4"><a href="#"><h3 className="font-semibold">About</h3></a></li>
+                                <li className="my-4"><a href="#"><h3 className="font-semibold">Contact</h3></a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            )}
         </div>
         
     )
