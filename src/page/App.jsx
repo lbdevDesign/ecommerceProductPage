1ac933635a61efbc67c0c21caa5064c7b4a31e26
Lucas <lucasbombaron@gmail.com>
2024-03-05 17:43:28 +0100

    media query nav bar

--- src/page/App.jsx
+++ src/page/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { useMediaQuery } from "@mui/material";
 
 //components
-import NavBar from "../components/navBar";
+import NavBarMobile from "../components/navBarMobile";
+import NavBarDesktop from "../components/navBarDesktop";
 
 function App() {
+  const mobile = useMediaQuery('(max-width:768px)');
+  const desktop = useMediaQuery('(min-width:768px)');
   return (
-    <div className="App">
-      <NavBar />
+    <div className="max-w-screen-lg mx-auto">
+      {mobile && <NavBarMobile /> }
+      {desktop && <NavBarDesktop /> }
     </div>
   );
 }
