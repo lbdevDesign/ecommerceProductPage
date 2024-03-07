43a8472e0b56b5ff3daf247ce3aa6db607003e8c
Lucas <lucasbombaron@gmail.com>
2024-03-07 17:33:51 +0100

    product info mobile update

--- src/components/productInfo.jsx
+++ src/components/productInfo.jsx
@@ -5,7 +5,7 @@ function ProductInfo(props) {
         <div className="py-4 px-6">
             <h2 className="text-orange-default uppercase font-semibold mb-4">{props.data.brand}</h2>
             <h1 className="text-neutral-very-dark-blue capitalize font-bold text-3xl mb-4">{props.data.title}</h1>
-            <p className="text-neutral-dark-grayish-blue text-m mb-4">{props.data.description}</p>
+            <p className="text-neutral-dark-grayish-blue text-m mb-6">{props.data.description}</p>
             <section className="flex items-center mb-16">
                 <p className="text-neutral-very-dark-blue capitalize font-bold text-3xl mr-4">${props.data.promPrice.toFixed(2)}</p>
                 <section className="bg-orange-pale px-2 py-1.5 rounded-lg h-fit mr-auto">
