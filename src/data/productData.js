d3415750c34d43b2d9433ce16c280db7ce1a5630
Lucas <lucasbombaron@gmail.com>
2024-03-07 16:52:10 +0100

    product data

---
+++ src/data/productData.js
@@ -0,0 +1,47 @@
+const productData = {
+    id: 1,
+    brand: "Sneaker Company",
+    title: "Fall limited edition sneakers",
+    description: "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+    price: 250.00,
+    promotion: "50",
+    promPrice: 125.00,
+    img: [
+        {
+            id: 1,
+            path: "/assets/image-product-1.jpg"
+        },
+        {
+            id: 2,
+            path: "/assets/image-product-2.jpg"
+        },
+        {
+            id: 3,
+            path: "/assets/image-product-3.jpg"
+        },
+        {
+            id: 4,
+            path: "/assets/image-product-4.jpg"
+        }
+    ],
+    thumbnail: [
+        {
+            id: 1,
+            path: "/assets/image-product-1-thumbnail.jpg"
+        },
+        {
+            id: 2,
+            path: "/assets/image-product-2-thumbnail.jpg"
+        },
+        {
+            id: 3,
+            path: "/assets/image-product-3-thumbnail.jpg"
+        },
+        {
+            id: 4,
+            path: "/assets/image-product-4-thumbnail.jpg"
+        }
+    ]
+}
+
+export default productData;
