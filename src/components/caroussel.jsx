4754c1c6b134f744afc6cf055bf094c1d7bb1216
Lucas <lucasbombaron@gmail.com>
2024-03-12 16:30:51 +0100

    responsive caroussel

--- src/components/caroussel.jsx
+++ src/components/caroussel.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import { useMediaQuery } from "@mui/material";
 
 //assets
 import nextIcon from "../assets/icon-next.svg";
 import previousIcon from "../assets/icon-previous.svg";
 
 function Caroussel(props) {
+    const mobile = useMediaQuery('(max-width:768px)');
+    const desktop = useMediaQuery('(min-width:768px)');
+
     const [imgIndex, setImgIndex] = useState(0);
     let imgShownPath = props.data.img[imgIndex].path
 
@@ -23,18 +27,57 @@ function Caroussel(props) {
                 setImgIndex(imgIndex - 1)
             }
     }
+
+    function clickIndex(index) {
+        setImgIndex(index)
+    }
     
     return(
-        <div className="w-screen relative z-0">
-            <img src={imgShownPath} alt={`Product ${imgIndex + 1}`}/>
-            <button onClick={prevIndex} className="absolute top-1/2 left-4 w-12 h-12 text-center bg-white rounded-full" >
-                <img src={previousIcon} className="m-auto" alt="Previous"/>
-            </button>
-            <button onClick={nextIndex} className="absolute top-1/2 right-4 w-12 h-12 text-center bg-white rounded-full" >
-                <img src={nextIcon}  className="m-auto" alt="Next"/>
-            </button>
+        <div className="md:mt-8 md:max-w-md md:p-8">
+            {mobile && (
+                <div className="w-screen relative z-0">
+                    <img src={imgShownPath} alt={`Product ${imgIndex + 1}`}/>
+                    <button onClick={prevIndex} className="absolute top-1/2 left-4 w-12 h-12 text-center bg-white rounded-full" >
+                        <img src={previousIcon} className="m-auto" alt="Previous"/>
+                    </button>
+                    <button onClick={nextIndex} className="absolute top-1/2 right-4 w-12 h-12 text-center bg-white rounded-full" >
+                        <img src={nextIcon}  className="m-auto" alt="Next"/>
+                    </button>
+                </div>
+            )}
+            {desktop && (
+                <div className="w-full">
+                    <img src={imgShownPath} alt={`Product ${imgIndex + 1}`} className="rounded-2xl"/>
+                    <div className="mt-3 flex">
+                        {props.data.img.map((button, index) => {
+                            if (index === imgIndex) {
+                                return (
+                                    <button 
+                                        key={button.id} 
+                                        className="w-full rounded-lg m-3 border-2 border-orange-default" 
+                                        onClick={() => clickIndex(index)}>
+                                            <img src={button.path} className="opacity-50"/>
+                                    </button>
+                                )
+                            } else {
+                                return (
+                                    <button 
+                                        key={button.id} 
+                                        className="w-full m-3 rounded-lg" 
+                                        onClick={() => clickIndex(index)}>
+                                            <img src={button.path} className="rounded-lg"/>
+                                    </button>
+                                )
+                            }
+
+                        })}
+                    </div>
+                </div>
+            )}
         </div>
+        
     )
 }
+
 
 export default Caroussel;
